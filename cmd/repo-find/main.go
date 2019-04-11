6cb97815ecf10772fc0dd56a6314a98cd10fd8c6
Denis Cheremisov <denis.cheremisov@ncloudtech.ru>
2019-04-11 14:54:10 +0300

    repos by branch finder

---
+++ cmd/repo-find/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/alexflint/go-arg"
+	"github.com/sirkon/message"
+
+	"github.com/sirkon/cmd-tools/internal/git"
+)
+
+var args struct {
+	Branch string `arg:"positional,required"`
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	gopath := os.Getenv("GOPATH")
+	if len(gopath) == 0 {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			message.Fatal(err)
+		}
+		gopath = filepath.Join(home, "go")
+	}
+
+	gosrc := filepath.Join(gopath, "src")
+
+	action := action{
+		branch: args.Branch,
+		gopath: gopath,
+		gosrc:  gosrc,
+		prefix: gosrc + string(filepath.Separator),
+	}
+	if err := filepath.Walk(gosrc, action.action); err != nil {
+		message.Fatal(err)
+	}
+}
+
+type action struct {
+	branch string
+	gopath string
+	gosrc  string
+	prefix string
+}
+
+func (a *action) action(path string, info os.FileInfo, err error) error {
+	if !info.IsDir() {
+		return nil
+	}
+	if path == a.gosrc {
+		return nil
+	}
+	if !strings.HasPrefix(path, a.prefix) {
+		message.Warningf("unexpected path `%s` for given go path `%s`", path, a.gopath)
+		return filepath.SkipDir
+	}
+
+	project := path[len(a.prefix):]
+	items := strings.Split(project, string(filepath.Separator))
+
+	if len(items) < 3 {
+		return nil
+	}
+	if len(items) > 3 {
+		return filepath.SkipDir
+	}
+
+	if err := os.Chdir(path); err != nil {
+		message.Fatalf("failed to chdir into %s: %s", path, err)
+	}
+
+	res, err := git.Do("branch")
+	if err != nil {
+		message.Warningf("warning: project `%s` is not under git control", project)
+		return filepath.SkipDir
+	}
+
+	scanner := bufio.NewScanner(res)
+	var be Branch
+	for scanner.Scan() {
+		if ok, _ := be.Extract(scanner.Bytes()); !ok {
+			message.Fatalf("failed to process branch line `%s`", scanner.Text())
+		}
+		if a.branch == be.Name {
+			message.Info(project)
+		}
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/sirkon/cmd-tools
 require (
 	github.com/StackExchange/wmi v0.0.0-20181212234831-e0a55b97c705 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/alexflint/go-arg v1.0.0
 	github.com/go-ole/go-ole v1.2.4 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sanity-io/litter v1.1.0
 	github.com/shirou/gopsutil v2.18.12+incompatible
 	github.com/shirou/w32 v0.0.0-20160930032740-bb4de0191aa4 // indirect
