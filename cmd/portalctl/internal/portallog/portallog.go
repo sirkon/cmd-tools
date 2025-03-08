c468f7dccc7a85e6f3e48c9fec17d70b8c95bcb9
Denis Cheremisov <denis.cheremisov@gmail.com>
2025-03-08 19:52:10 +0300

    feat: portalctl moved to its own repo

--- cmd/portalctl/internal/portallog/portallog.go
+++
@@ -1,7 +0,0 @@
-package portallog
-
-// PortalLogger логирование операций над хранилищем порталов.
-type PortalLogger interface {
-	AddPortal(name string, path string) error
-	DeletePortal(name string) error
-}
