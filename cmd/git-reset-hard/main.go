91ad0925ae0372ae53c7f14221543e6c627b1fba
Denis <denis.cheremisov@gmail.com>
2025-11-29 04:58:57 +0300

    Refactor git reset hard command implementation

--- cmd/git-reset-hard/main.go
+++ cmd/git-reset-hard/main.go
@@ -2,27 +2,37 @@ package main
 
 import (
 	"bytes"
-	"github.com/sirkon/errors"
-	"github.com/sirkon/message"
 	"os/exec"
 	"strings"
+
+	"github.com/sirkon/errors"
+	"github.com/sirkon/message"
 )
 
 func main() {
-	cmd := exec.Command("git", "branch", "--show-current")
-	var tmpBuffer bytes.Buffer
-	cmd.Stdout = &tmpBuffer
-	if err := cmd.Run(); err != nil {
+	// Get current branch
+	out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+	if err != nil {
 		message.Fatal(errors.Wrap(err, "get current branch"))
 	}
-	branch := strings.TrimSpace(tmpBuffer.String())
+	branch := strings.TrimSpace(string(out))
 
-	if err := exec.Command("git", "fetch").Run(); err != nil {
-		message.Fatal(errors.Wrap(err, "fetch remote changes"))
+	// Check if remote branch exists
+	if branch != "HEAD" {
+		check, _ := exec.Command("git", "ls-remote", "--heads", "origin", branch).Output()
+		if len(check) == 0 {
+			message.Fatal(errors.Newf("remote branch origin/%s does not exist", branch))
+		}
 	}
 
-	if err := exec.Command("git", "reset", "--hard", "origin/"+branch).Run(); err != nil {
-		message.Fatal(errors.Wrap(err, "move HEAD to the state of the remote"))
+	// Fetch
+	if out, err := exec.Command("git", "fetch").CombinedOutput(); err != nil {
+		message.Fatal(errors.Wrap(err, "fetch remote changes: "+string(out)))
+	}
+
+	// Reset
+	if out, err := exec.Command("git", "reset", "--hard", "origin/"+branch).CombinedOutput(); err != nil {
+		message.Fatal(errors.Wrap(err, "reset failed: "+string(out)))
 	}
 
 	message.Info("done")
