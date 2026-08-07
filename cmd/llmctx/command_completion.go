72cd9f1753b5a2bdb03f73edb043f8242cdd59d6
Denis Cheremisov <denis.cheremisov@gmail.com>
2026-08-07 18:45:41 +0300

    feat: save completion right into completion scripts directories

--- cmd/llmctx/command_completion.go
+++ cmd/llmctx/command_completion.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/sirkon/errors"
+	"github.com/sirkon/message"
 
 	"github.com/sirkon/cmd-tools/cmd/llmctx/internal/complscript"
 )
 
 // CommandCompletion сгенерировать скрипт автодополнения.
 type CommandCompletion struct {
-	Shell ShellType `arg:"" help:"Shell type to generate completion script for."`
+	Shell ShellType `arg:""    help:"Shell type to generate completion script for."`
+	Save  bool      `short:"s" help:"Save completion script for llmctx within completion files."`
 }
 
 func (c *CommandCompletion) Run(ctx *runContext) error {
@@ -22,7 +28,41 @@ func (c *CommandCompletion) Run(ctx *runContext) error {
 		complete = complscript.Fish
 	}
 
-	fmt.Println(complete)
+	if !c.Save {
+		fmt.Println(complete)
+		return nil
+	}
+
+	complFileName := shellComplFileName(c.Shell, appName)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return errors.Wrap(err, "get current user home directory")
+	}
+
+	var absComplRoot string
+	switch c.Shell {
+	case shellTypeBash:
+		absComplRoot = filepath.Join(home, ".local", "share", "bash-completion")
+	case shellTypeZsh:
+		absComplRoot = filepath.Join(home, ".zsh")
+	case shellTypeFish:
+		cfgDir, err := os.UserConfigDir()
+		if err != nil {
+			return errors.Wrap(err, "get current user config directory")
+		}
+		absComplRoot = filepath.Join(cfgDir, "fish")
+	}
+	absComplFileName := filepath.Join(absComplRoot, "completions", complFileName)
+
+	if err := os.MkdirAll(filepath.Dir(absComplFileName), 0755); err != nil {
+		return errors.Wrap(err, "create completion directory")
+	}
+	if err := os.WriteFile(absComplFileName, []byte(complete), 0644); err != nil {
+		return errors.Wrap(err, "write completion script")
+	}
+
+	message.Infof("Completion script saved to %s\n", absComplFileName)
+
 	return nil
 }
 
@@ -47,4 +87,18 @@ func (s *ShellType) UnmarshalText(text []byte) error {
 	}
 
 	return nil
+}
+
+// shellComplFileName базовое имя файла.
+func shellComplFileName(shell ShellType, name string) string {
+	switch shell {
+	case shellTypeZsh:
+		return "_" + name
+	case shellTypeBash:
+		return name
+	case shellTypeFish:
+		return name + ".fish"
+	default:
+		return "unknown"
+	}
 }
